585091ea79c2cb6fb64d3cd5e4a691b1e78bbff5
root <root@DESKTOP-Q7G2NQ5.>
2025-06-14 15:24:21 +0000

    Add terraform files back to repository

---
+++ terraform/modules/cloudfront/variables.tf
@@ -0,0 +1,9 @@
+variable "acm_arn" {
+  description = "ACM Certificate ARN"
+  type        = string
+}
+
+variable "s3_bucket" {
+  description = "S3 Regional Domain Name"
+  type        = string
+}
